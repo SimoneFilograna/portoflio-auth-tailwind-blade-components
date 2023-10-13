342b6738b24adc34814fdd16d3ccb458131b1fba
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-13 18:43:37 +0200

    update-guest-second-page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,13 +17,8 @@
     <body class="font-sans text-gray-900 antialiased">
         <video src="/bg-video/bg-vid.mp4" autoplay loop muted class="bg-vid"></video>
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 relative">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
             
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full ">
                 {{ $slot }}
             </div>
         </div>
