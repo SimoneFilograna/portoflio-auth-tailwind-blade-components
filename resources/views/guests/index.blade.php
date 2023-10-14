b95f37e1f68344c1d7e72ee5504230d6518cb615
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-14 03:06:44 +0200

    add-animation-photo

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -30,8 +30,10 @@
 
             <div class="columns-2 flex justify-center gap-48 items-center">
                 <div class="portfolio-text basis-3/6">
-                    
-                    <h1 class="text-4xl pb-3">Welcome to My portfolio</h1>
+                    <div class="img-container w-64 rounded-full overflow-hidden relative">
+                        <img src="/img/nc.jpg" class="w-full relative z-50 rounded-full p-2" alt="">
+                    </div>
+                    <h1 class="text-4xl mt-4 pb-3">Welcome to My portfolio</h1>
                     <p>Hello, I am Simone, a passionate web developer who has been immersed in the world of IT since the age of 15. Thanks to my curiosity, I have had the opportunity to delve into various programming languages, always striving to learn and preferring logic over memorization. There is no memory that can surpass a well-structured reasoning.</p>
                 </div>
 
